0ae53f0151d5c13c5703933606c5ca90d40776e3
Oleksii Klymenko <alex.klimenko@gmail.com>
2023-02-26 19:21:40 +0200

    split App to several components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {Product} from "./components/Product";
+import {Loader} from "./components/Loader"
+import {ErrorMessage} from "./components/ErrorMessage"
 import {useProducts} from "./hooks/products";
 
 function App() {
     const [products, isLoading, error] = useProducts()
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            { isLoading && <p className="text-center">Loading...</p> }
-            { error && <p className="text-center text-red-600">{ error }</p> }
+            { isLoading && <Loader/> }
+            { error && <ErrorMessage error={error}/> }
             { products.map(product => <Product product={product} key={product.id} />) }
         </div>
     )
