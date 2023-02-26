c9ddcb709c3a4fa9e251dc772d2b6a5155bc3e4c
Oleksii Klymenko <alex.klimenko@gmail.com>
2023-02-26 18:14:48 +0200

    add IProduct interface and fakestoreapi.com data stub

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,12 @@
-export function Product() {
+import React from "react";
+import {IProduct} from "../models";
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product(props: ProductProps) {
     return (
-        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">Product</div>
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">{props.product.title}</div>
     )
 }
