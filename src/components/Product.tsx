9677fe6b598f9431f7bd4ad3b1d03b1726359167
Oleksii Klymenko <alex.klimenko@gmail.com>
2023-02-26 18:20:50 +0200

    add some styles to Product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -5,8 +5,13 @@ interface ProductProps {
     product: IProduct
 }
 
-export function Product(props: ProductProps) {
+export function Product({product}: ProductProps) {
     return (
-        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">{props.product.title}</div>
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src={product.image} className="w-1/6" alt={product.title} />
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}</p>
+            <p>{product.description}</p>
+        </div>
     )
 }
