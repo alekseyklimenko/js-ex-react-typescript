8eaa765c8477e60c108f6a639af2193863255962
Oleksii Klymenko <alex.klimenko@gmail.com>
2023-02-27 09:07:35 +0200

    send post request from modal form

--- src/components/CreateProductForm.tsx
+++ src/components/CreateProductForm.tsx
@@ -1,9 +1,31 @@
 import React, {useState} from "react";
+import {IProduct} from "../models";
+import axios from "axios";
+import {ErrorMessage} from "./ErrorMessage";
 
 export function CreateProductForm() {
+    const productData: IProduct = {
+        title: 'test product',
+        price: 13.5,
+        description: 'lorem ipsum set',
+        image: 'https://i.pravatar.cc',
+        category: 'electronic',
+        rating: {
+            rate: 42,
+            count: 10
+        }
+    }
     const [value, setValue] = useState('')
+    const [error, setError] = useState('')
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault()
+        setError('')
+        if (value.trim().length === 0) {
+            setError('Please enter valid title')
+            return
+        }
+        productData.title = value
+        const resp = axios.post<IProduct>('https://fakestoreapi.com/products', productData)
     }
     const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value)
@@ -17,6 +39,7 @@ export function CreateProductForm() {
                    onChange={changeHandler}
             />
 
+            { error && <ErrorMessage error={error}/> }
             <button type="submit" className="py-2 px-4 border bg-yellow-400">Create</button>
         </form>
     )
